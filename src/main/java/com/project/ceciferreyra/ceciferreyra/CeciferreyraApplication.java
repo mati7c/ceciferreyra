a080b13e4a8c1e1a99f1f37d87f27d580c205856
Walter Matias Castro <matic1087@gmail.com>
2025-04-07 20:57:42 -0300

    Mejorado

--- src/main/java/com/project/ceciferreyra/ceciferreyra/CeciferreyraApplication.java
+++ src/main/java/com/project/ceciferreyra/ceciferreyra/CeciferreyraApplication.java
@@ -1,10 +1,21 @@
 package com.project.ceciferreyra.ceciferreyra;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CeciferreyraApplication {
+
+	@PostConstruct
+	public void loadEnv() {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> {
+			// Carga cada variable al entorno
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CeciferreyraApplication.class, args);
