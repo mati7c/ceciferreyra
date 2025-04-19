fa1d2ca1936cc660a73f365713e9454314062ffc
Walter Matias Castro <matic1087@gmail.com>
2025-04-19 14:35:03 -0300

    login bien

--- src/main/java/com/project/ceciferreyra/ceciferreyra/config/SecurityConfig.java
+++ src/main/java/com/project/ceciferreyra/ceciferreyra/config/SecurityConfig.java
@@ -20,7 +20,11 @@ import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequ
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -30,6 +34,19 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://ceciferreyraart.vercel.app"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "https://ceciferreyraart.vercel.app"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true); // 🔥 Esto es clave
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Set-Cookie")); // Opcional pero útil
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, ClientRegistrationRepository clientRegistrationRepository) throws Exception {
 
@@ -61,7 +78,7 @@ public class SecurityConfig {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(withDefaults())
+                .cors(c -> c.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/piece/list").permitAll()
                         .requestMatchers(HttpMethod.GET, "/piece/list/**").permitAll()
@@ -87,7 +104,7 @@ public class SecurityConfig {
                                 .authorizationRequestResolver(customAuthorizationRequestResolver)
                         )
                         .userInfoEndpoint(userInfo -> userInfo.oidcUserService(this.oidcUserService()))
-                        .defaultSuccessUrl("https://ceciferreyraart.vercel.app//login", true)
+                        .defaultSuccessUrl("https://ceciferreyraart.vercel.app/login", true)
                 );
 
         return http.build();
