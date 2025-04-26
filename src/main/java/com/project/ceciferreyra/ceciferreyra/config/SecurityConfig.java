ea0069e39720f218ae144b20f3f2f6fb9601bc1c
Walter Matias Castro <matic1087@gmail.com>
2025-04-26 15:45:06 -0300

    TEST

--- src/main/java/com/project/ceciferreyra/ceciferreyra/config/SecurityConfig.java
+++ src/main/java/com/project/ceciferreyra/ceciferreyra/config/SecurityConfig.java
@@ -11,19 +11,19 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import java.util.*;
 
 @Configuration
 @EnableWebSecurity
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://ceciferreyraart.vercel.app", "http://localhost:3000","https://accounts.google.com"));
+        configuration.setAllowedOrigins(Arrays.asList("https://ceciferreyraart.vercel.app", "http://localhost:3000", "https://github.com"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
@@ -41,6 +41,7 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -68,7 +69,11 @@ public class SecurityConfig {
                         .deleteCookies("JSESSIONID")
                 )
                 .oauth2Login(oauth2 -> oauth2
-                        .userInfoEndpoint(userInfo -> userInfo.oidcUserService(this.oidcUserService()))
+                        .userInfoEndpoint(userInfo -> {
+                            userInfo
+                                    .oidcUserService(this.oidcUserService())    // Para Google
+                                    .userService(this.oauth2UserService());    // Para GitHub
+                        })
                         .defaultSuccessUrl("https://ceciferreyraart.vercel.app/login", true)
                 );
 
@@ -77,11 +82,10 @@ public class SecurityConfig {
 
     private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
         final OidcUserService delegate = new OidcUserService();
-
         return userRequest -> {
             OidcUser oidcUser = delegate.loadUser(userRequest);
             String email = oidcUser.getEmail();
-            System.out.println("Usuario autenticado: " + email);
+            System.out.println("Usuario autenticado (Google): " + email);
 
             Set<GrantedAuthority> authorities = new HashSet<>();
             if ("matic1087@gmail.com".equals(email) || "ceciliaferreyra74@gmail.com".equals(email)) {
@@ -89,6 +93,26 @@ public class SecurityConfig {
             }
 
             return new DefaultOidcUser(authorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+        };
+    }
+
+    private OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+        return userRequest -> {
+            OAuth2User oauth2User = delegate.loadUser(userRequest);
+            String login = (String) oauth2User.getAttributes().get("login");
+            System.out.println("Usuario autenticado (GitHub login): " + login);
+
+            Set<GrantedAuthority> authorities = new HashSet<>();
+            if ("mati7c".equals(login) || "ceciferreyra74".equals(login)) {
+                authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+            }
+
+            return new DefaultOAuth2User(
+                    authorities,
+                    oauth2User.getAttributes(),
+                    "login" // Atributo principal
+            );
         };
     }
 }
